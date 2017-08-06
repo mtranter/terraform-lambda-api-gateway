ca630e89355aea505ad2fdf6d586b64ad8e66742
Mark Tranter <mark.a.tranter@gmail.com>
2017-08-06 09:47:17 +0100

    Initial commit

---
+++ module/main.tf
@@ -0,0 +1,60 @@
+resource "aws_lambda_function" "lambda_function" {
+  filename          = "${var.source_file}"
+  s3_bucket         = "${var.source_s3_bucket}"
+  s3_key            = "${var.source_s3_key}"
+  s3_object_version = "${var.source_s3_object_version}"
+  function_name     = "${var.function_name}"
+  role              = "${aws_iam_role.lambda_role.arn}"
+  handler           = "${var.handler}"
+  runtime           = "${var.runtime}"
+  source_code_hash  = "${base64sha256(file("${data.archive_file.lambda.output_path}"))}"
+  publish           = true
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "${coalesce(var.role_name, format("%s-role", var.function_name)) }"
+  assume_role_policy = "${var.role_policy}"
+}
+
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = "${var.rest_api_id}"
+  parent_id   = "${var.parent_id}"
+  path_part   = "${var.path_part}"
+}
+
+resource "aws_api_gateway_method" "api_method" {
+  rest_api_id   = "${var.rest_api_id}"
+  resource_id   = "${aws_api_gateway_resource.api_resource.id}"
+  http_method   = "${var.http_method}"
+  authorization = "${var.authorization}"
+  authorizer_id = "${var.authorizer_id}"
+}
+
+resource "aws_api_gateway_integration" "api_method_integration" {
+  rest_api_id             = "${var.rest_api_id}"
+  resource_id             = "${aws_api_gateway_resource.api_resource.id}"
+  http_method             = "${aws_api_gateway_method.api_method.http_method}"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.lambda_function.invoke_arn}"
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    "aws_api_gateway_method.api_method",
+    "aws_api_gateway_integration.api_method_integration",
+  ]
+
+  rest_api_id = "${var.rest_api_id}"
+  stage_name  = "${var.stage_name}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda_function.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/${aws_api_gateway_deployment.deployment.stage_name}/${var.http_method}${aws_api_gateway_resource.api_resource.path}"
+}
---
+++ module/output.tf
@@ -0,0 +1,35 @@
+output "invoke_url" {
+  value = "${aws_api_gateway_deployment.deployment_prod.invoke_url}"
+}
+
+output "aws_lambda_function_arn" {
+  value = "${aws_lambda_function.lambda_function.arn}"
+}
+
+output "aws_lambda_function_invoke_arn" {
+  value = "${aws_lambda_function.lambda_function.invoke_arn}"
+}
+
+output "aws_lambda_function_version" {
+  value = "${aws_lambda_function.lambda_function.version}"
+}
+
+output "aws_api_gateway_resource_id" {
+  value = "${aws_api_gateway_resource.api_resource.id}"
+}
+
+output "aws_api_gateway_resource_path" {
+  value = "${aws_api_gateway_resource.api_resource.path}"
+}
+
+output "aws_api_gateway_deployment_id" {
+  value = "${aws_api_gateway_deployment.deployment.id}"
+}
+
+output "aws_api_gateway_deployment_invoke_url" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}"
+}
+
+output "aws_api_gateway_deployment_execution_arn" {
+  value = "${aws_api_gateway_deployment.deployment.invoke_url}"
+}
