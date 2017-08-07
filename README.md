a7966396b7e3045cc4af4853a625218377f4fdc0
Mark Tranter <mark.a.tranter@gmail.com>
2017-08-07 20:27:17 +0100

    Crank up timeout... clearly dotnet is slow to kick off on lambda. heads up

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Terraform module for AWS Lambda functions fronted by API Gateway configured with AWS_PROXY integration type.
 
-Examples provided for Node and Java.
+Examples provided for Node, Java and DotNet.
--- examples/dotnet/deploy/module.tf
+++ examples/dotnet/deploy/module.tf
@@ -12,18 +12,19 @@ resource "aws_api_gateway_rest_api" "test_dotnet_api" {
 }
 
 module "dotnet-example" {
-  source        = "github.com/mtranter/terraform-lambda-api-gateway//module"
-  source_file   = "./../src/LambdaDotNet/bin/Release/netcoreapp1.0/LambdaDotNet.zip"
-  function_name = "dotnet-example"
-  runtime       = "dotnetcore1.0"
-  handler       = "LambdaDotNet::Trizzle.Lambda::Handler"
-  stage_name    = "prod"
-  account_id    = "277618971297"
-  rest_api_id   = "${aws_api_gateway_rest_api.test_dotnet_api.id}"
-  parent_id     = "${aws_api_gateway_rest_api.test_dotnet_api.root_resource_id}"
-  path_part     = "hellolambdadotnet"
-  http_method   = "GET"
-  region        = "${var.region}"
+  source           = "github.com/mtranter/terraform-lambda-api-gateway//module"
+  source_file      = "./../src/LambdaDotNet/bin/Release/netcoreapp1.0/LambdaDotNet.zip"
+  function_name    = "dotnet-example"
+  runtime          = "dotnetcore1.0"
+  handler          = "LambdaDotNet::Trizzle.Lambda::Handler"
+  stage_name       = "prod"
+  account_id       = "277618971297"
+  rest_api_id      = "${aws_api_gateway_rest_api.test_dotnet_api.id}"
+  parent_id        = "${aws_api_gateway_rest_api.test_dotnet_api.root_resource_id}"
+  path_part        = "hellolambdadotnet"
+  http_method      = "GET"
+  region           = "${var.region}"
+  function_timeout = "30"
 }
 
 output "invoke_url" {
