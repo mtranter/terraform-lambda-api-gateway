cec26d70d435ff11bf3cc5aee5bf29cc9a714260
Mark Tranter <mark.a.tranter@gmail/com>
2018-02-02 17:16:50 +1100

    Fix permissions src arn

--- module/main.tf
+++ module/main.tf
@@ -50,11 +50,13 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
+
+  count         = "${length(var.http_methods)}"
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda_function.arn}"
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/${aws_api_gateway_deployment.deployment.stage_name}/${var.http_method}${aws_api_gateway_resource.api_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.rest_api_id}/${aws_api_gateway_deployment.deployment.stage_name}/${element(var.http_methods, count.index)}${aws_api_gateway_resource.api_resource.path}"
 }
