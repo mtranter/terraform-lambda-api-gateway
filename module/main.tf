a4967788f329642c431425e86021d1545ee445a5
Mark Tranter <mark.a.tranter@gmail/com>
2018-02-03 13:45:44 +1100

    Remove required sub resource

--- module/main.tf
+++ module/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_api_gateway_resource" "api_resource" {
-  count       = "${var.use_sub_resourse == "true" ? 1 : 0}"
+  count       = "${var.use_sub_resource == "true" ? 1 : 0}"
   rest_api_id = "${var.rest_api_id}"
   parent_id   = "${var.parent_id}"
   path_part   = "${var.path_part}"
@@ -33,7 +33,7 @@ resource "aws_api_gateway_method" "api_method" {
 resource "aws_api_gateway_integration" "api_method_integration" {
   count                   = "${length(var.http_methods)}"
   rest_api_id             = "${var.rest_api_id}"
-  resource_id             = "${var.use_sub_resourse == "true" ? aws_api_gateway_resource.api_resource.id : var.parent_id}"
+  resource_id             = "${var.use_sub_resource == "true" ? aws_api_gateway_resource.api_resource.id : var.parent_id}"
   http_method             = "${element(aws_api_gateway_method.api_method.*.http_method, count.index)}"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.lambda_function.invoke_arn}"
